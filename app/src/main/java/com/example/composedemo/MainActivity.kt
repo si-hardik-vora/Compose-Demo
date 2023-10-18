d55b22c857b26cb56a4bf80f3656b7801193986d
hardik41 <Hardik.Vora@sportzinteractive.net>
2023-10-18 18:22:18 +0530

    Side Effects, Remember Coroutine Scope & Launch effect example

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -3,6 +3,7 @@
 package com.example.composedemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -20,8 +21,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,16 +36,110 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            PreviewItem()
-            previewComposDemos()
+//          PreviewItem()
+            CoroutineScopeComposable()
 
         }
     }
+}
+
+@Preview
+@Composable
+fun CoroutineScopeComposable() {
+
+    val count = rememberSaveable { mutableStateOf(0) }
+    val buttonText = rememberSaveable { mutableStateOf("Start") }
+    val scope = rememberCoroutineScope()
+    var text = "Counter is running ${count.value} "
+
+    if (count.value == 10) {
+        count.value = 0
+        text = "Counter stopped"
+        buttonText.value = "Start"
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Button(
+            onClick = {
+                buttonText.value = "Started"
+                scope.launch {
+                    Log.e("CoroutineScopeComposable", "Started: ")
+                    try {
+                        for (i in 1..10) {
+                            count.value++
+                            delay(1000)
+                        }
+                    } catch (e: Exception) {
+                        Log.e("CoroutineScopeComposable", "Exception - ${e.message.toString()} ")
+                    }
+                }
+            },
+            modifier = Modifier
+                .padding(top = 15.dp)
+                .size(width = 150.dp, height = 50.dp)
+        ) {
+            Text(text = buttonText.value)
+        }
+    }
+}
+
+
+/*TODO
+*  Here is the example of Side effect,
+*  Side effect means What ever changes which can be out side of
+*  composable function scope known as Side effect
+*  we can easily manage side effect
+*  with the help of LaunchedEffect */
+@Composable
+fun LaunchEffect() {
+
+    val count = rememberSaveable { mutableStateOf(0) }
+    var key = count.value % 3 == 0
+//    Log.e("Count", "LaunchEffect: ${count.value}" )
+
+    LaunchedEffect(key1 = key) {
+        Log.e("Count", "LaunchEffect: ${count.value}")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 20.dp, end = 20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Current count ${count.value}",
+            color = Color.Blue
+        )
+
+        Button(
+            onClick = { count.value++ },
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Click here",
+                color = Color.Cyan
+            )
+        }
+
+
+    }
+
 }
 
 @Composable
@@ -100,7 +197,6 @@ fun listViewItem(imgId: Int, name: String, occupation: String) {
 }
 
 
-@Preview(showBackground = true, name = "message", showSystemUi = true)
 @Composable
 fun previewComposDemos() {
     //Image Composable
